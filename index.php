85b40502038c20282d411f2ac39327e3852d97ec
Ryoma Wakita <your.ryoma.wakita@gmail.com>
2015-08-16 17:23:46 +0800

    develop index.php(HTML)

---
+++ index.php
@@ -0,0 +1,25 @@
+<?php
+
+
+?>
+
+<!DOCTYPE html>
+<html lang="ja">
+	<head>
+		<meta charset="utf-8">
+		<title>簡易掲示板</title>
+	</head>
+	<body>
+		<h1>簡易掲示板</h1>
+		<form>
+			Message: <input type="text" name="message">
+			User: <input type="text" name="user">
+			<input type="submit" value="投稿">
+
+		</form>
+		<h2>投稿一覧（0件）</h2>
+		<ul>
+			<li>まだ投稿はありません。</li>
+		</ul>
+	</body>
+</html>
